9948fc65bc9321d72868019cae66278159c6dd22
Romário Arruda <romarioarruda98@gmail.com>
2022-01-14 15:50:30 -0300

    oo - interfaces

--- oo/main.go
+++ oo/main.go
@@ -6,10 +6,21 @@ import (
 	// "go-alura/oo/clientes"
 )
 
+type Conta interface {
+	Sacar(valor float64) string
+}
+
+func PagarBoleto(conta Conta, valorDoBoleto float64) {
+	status := conta.Sacar(valorDoBoleto)
+
+	fmt.Println(status)
+}
+
 func main() {
 	contaCliente := contas.ContaCorrente{}
-
 	contaCliente.Deposito(100)
+
+	PagarBoleto(&contaCliente, 60)
 
 	fmt.Println(contaCliente.Saldo())
 }
