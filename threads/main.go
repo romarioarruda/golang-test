9905c369b0b3b21f0d273d18c10bc8e4137f884c
Romário Arruda | Dev <romarioarruda98@gmail.com>
2023-07-19 15:52:38 -0300

    adicionando <-channel pra esvaziar canais e channel <- i pra encher um canal

--- threads/main.go
+++ threads/main.go
@@ -7,18 +7,22 @@ import (
 
 func worker(workerId int, data chan int) {
 	for x := range data {
-		fmt.Printf("Worker %d received %d\n", workerId, x)
-		time.Sleep(time.Second * 5)
+		time.Sleep(time.Second)
+		fmt.Printf("Worker %d finished: %d\n", workerId, x)
 	}
 }
 
 //Thread 1
 func main() {
 	channel := make(chan int) //cria um canal para comunicar entre as threads
-	qtd := 1000000
+	qtd := 100
 	for i := 1; i <= qtd; i++ {
+		fmt.Printf("Worker %d\n", i)
+
 		go worker(i, channel) //Thread 2, 3, 4 etc...
 
-		channel <- i
+		channel <- i // enche o canal
 	}
+
+	fmt.Println(<-channel) // esvazia o canal
 }
